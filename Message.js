a48ea801fef3526a94cfa776a443b555e17ccd11
Nauman Ahmad <tnsd995@gmail.com>
2020-06-17 02:51:58 +0500

    add Message.js

---
+++ Message.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Message(props) {
+  return <h3>Value of counter variable is: {props.counter}</h3>;
+}
---
+++ src/Message.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Message(props) {
+  return <h2>Value is: {props.counter}</h2>;
+}
